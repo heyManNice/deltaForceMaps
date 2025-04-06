f1cd0ffaccbe203d274015f06f6a35c99d3fdbaa
heyManNice <3031978990@qq.com>
2025-04-06 21:16:04 +0800

    feat:图层组添加卫星图

--- src/tilesLoader/index.ts
+++ src/tilesLoader/index.ts
@@ -2,12 +2,16 @@ import L from 'leaflet';
 
 
 function enalbe(map:L.Map){
+    const layerGroup = L.layerGroup(void 0,{
+            pane: "卫星图",
+        });
+    layerGroup.addTo(map);
     L.tileLayer('./tiles/{z}/{x}/{y}.png', {
         minZoom: 15,
         maxZoom: 21,
         tileSize: 256,
         attribution: 'deltaForceMaps'
-    }).addTo(map);
+    }).addTo(layerGroup);
 }
 
 function disable(){
