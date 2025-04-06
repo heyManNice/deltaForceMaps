f5243fd22baa5e7fafdc47beca4c14243f1a7b85
heyManNice <3031978990@qq.com>
2025-04-06 11:44:20 +0800

    feat:添加搜索框

--- src/uiLayer/index.tsx
+++ src/uiLayer/index.tsx
@@ -1,11 +1,15 @@
 
 import style from "./style.module.css"
 import Navigation from "./navigation"
+import SearchBox from "./searchBox"
 
 
 export default function() {
     return (
-        <div className={style.top}>
+        <div className={style.screen}>
+            <div className={style.top}>
+                <SearchBox />
+            </div>
             <div className={style.bottom}>
                 <Navigation />
             </div>
