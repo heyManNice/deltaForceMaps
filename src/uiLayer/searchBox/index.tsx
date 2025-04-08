82bccf0a77fa1dc8c9ad6db8e926d7bb3dc8951f
heyManNice <3031978990@qq.com>
2025-04-08 15:09:23 +0800

    feat:动态搜索推荐词

--- src/uiLayer/searchBox/index.tsx
+++ src/uiLayer/searchBox/index.tsx
@@ -7,27 +7,48 @@ import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import DirectionsIcon from '@mui/icons-material/Directions';
 
-export default function() {
-  return (
-    <Paper
-      component="form"
-      sx={{ p: '4px 4px', display: 'flex', alignItems: 'center',flex:'1 0 0' ,marginTop:"1rem",marginLeft:"1rem",marginRight:"1rem"}}
-    >
-      <IconButton sx={{ p: '10px' }} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="最近的卫星刷新点在哪里?"
-        inputProps={{ 'aria-label': '最近的卫星刷新点在哪里?' }}
-      />
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
-    </Paper>
-  );
+import { Ref } from '@src/utils';
+import { useEffect } from 'react';
+
+const placeholders = [
+    "卫星锅刷新点在哪里?",
+    "大红!我要大红!",
+    "老六阴🖊位",
+    "跑刀路线"
+];
+
+function random(min:number,max:number){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export default function () {
+    const placeholderIndex = Ref(random(0, placeholders.length - 1));
+    useEffect(()=>{
+        const interval = setInterval(() => {
+            placeholderIndex.set(random(0, placeholders.length - 1));
+        }, 5000);
+        return () => clearInterval(interval);
+    });
+    return (
+        <Paper
+            component="form"
+            sx={{ p: '4px 4px', display: 'flex', alignItems: 'center', flex: '1 0 0', marginTop: "1rem", marginLeft: "1rem", marginRight: "1rem" }}
+        >
+            <IconButton sx={{ p: '10px' }} aria-label="menu">
+                <MenuIcon />
+            </IconButton>
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder={placeholders[placeholderIndex.val]}
+                inputProps={{ 'aria-label': '最近的卫星刷新点在哪里?' }}
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon />
+            </IconButton>
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+                <DirectionsIcon />
+            </IconButton>
+        </Paper>
+    );
 }
